8ea166866409ca4320cda4e0f6aba3d556be10e5
TheSpaceHuman <kolodyazhnyartem@gmail.com>
2019-01-09 01:48:03 +0300

    08-01-19
    index

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,33 @@
 //=======User js=============
+$(document).ready(function() {
+  $('.header_top_lang_item').click(function () {
+    // console.log( "clicked!" );
+    $('.header_top_lang_item').removeClass('active');
+    $(this).addClass('active');
+  });
+  //top-menu
+  $('.header_bottom_main-menu_item').mouseenter(function (event) {
+    $('.header_bottom_main-menu_item').removeClass('active');
+    $(this).addClass('active')
+  });
+  /*$('.header_bottom_main-menu_item').mouseleave(function (event) {
+    if ($(this).children().mouseenter(function () {
+      console.log( "hovel to children submenu" );
+    }))
+      console.log( "no hovel to children submenu" );
+    // $(this).removeClass('active');
+  })*/
+  //Slider
+  $('.big-slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // centerMode: true,
+    // variableWidth: true
+  });
+});
+
